107b10a325d64a5e7567644a5b4d13cab604c591
Justin Norman <endlessxaura@gmail.com>
2018-04-06 19:03:34 -0400

    Added the /user route
    The /user route returns an authenticated user's information

--- server/app/Http/Controllers/AuthenticatedController.php
+++ server/app/Http/Controllers/AuthenticatedController.php
@@ -11,10 +11,48 @@ class AuthenticatedController extends Controller
     * @return void
     */
     public function __construct(){
-        $this->middleware('jwt.auth');
+        $this->middleware('jwt.auth', 'jwt.refresh');
     }
 
-    public function test(){
-        return "GOOD JOB";
+    //General Functions
+    public function getUserObject() {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return NULL;
+            }
+            return $user;
+        } catch (Exception $e) {
+            return NULL;
+        }
+    }
+
+    //Route Functions  
+    public function getAuthenticatedUser() {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
+    }
+
+    public function employees(){
+
     }
 }
--- server/routes/web.php
+++ server/routes/web.php
@@ -18,32 +18,9 @@ Route::get('/', function () {
 });
 
 //Authentication
-Route::post('/getPassword', function(Request $request){
-     return Hash::make($request->password);
-});
-
 Route::post('/authenticate', 'AuthController@authenticate');
 
-Route::get('/test', 'AuthenticatedController@test');
+Route::get('/user', 'AuthenticatedController@getAuthenticatedUser');
 
-//Employer
-Route::get('/employer/{employer_id}', function ($employer_id){
-    $employer = DB::table('employer')->where('employer_id', $employer_id)->first();
-    return json_encode($employer);
-});
-
-Route::get('/employer/{employer_id}/employees', function ($employer_id){
-    $employees = DB::table('employee')->where('employer_id', $employer_id)->get();
-    return json_encode($employees);
-});
-
-Route::get('/employer/{employer_id}/jobs', function($employer_id){
-    $jobs = DB::table('job')->where('employer_id', $employer_id)->get();
-    return json_encode($jobs);
-});
-
-//Employee
-Route::get('employee/{employee_id}', function($employee_id){
-    $employee = DB::table('employee')->where('employee_id', $employee_id)->first();
-    return json_encode($employee);
-});
+//Employees
+Route::get('/employees', 'AuthenticatedController@employees');
